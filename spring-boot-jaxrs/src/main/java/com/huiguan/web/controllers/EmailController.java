a4e64c11b221ffc784916a7a9961a7389f800d50
e0008233 <howard.zhang.1995@gmail.com>
2019-11-26 21:07:09 +0800

    update api

---
+++ spring-boot-jaxrs/src/main/java/com/huiguan/web/controllers/EmailController.java
@@ -0,0 +1,37 @@
+package com.huiguan.web.controllers;
+
+import com.huiguan.web.dto.BaseResponse;
+import com.huiguan.web.dto.GetResourceResponse;
+import com.huiguan.web.exception.ApiException;
+import com.huiguan.web.service.EmailService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+@Component
+@Path("/email")
+public class EmailController extends Application {
+    private static final Logger logger = LogManager.getLogger(HomeController.class);
+
+    @Autowired
+    EmailService emailService;
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/")
+    @Transactional
+    public BaseResponse getResource() throws ApiException {
+        System.out.println("--------------------");
+        emailService.start();
+        BaseResponse response = new BaseResponse(200,true);
+        return response;
+    }
+}
---
+++ spring-boot-jaxrs/src/main/java/com/huiguan/web/service/EmailService.java
@@ -0,0 +1,72 @@
+package com.huiguan.web.service;
+
+
+import com.sun.mail.smtp.SMTPTransport;
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+@Service
+public class EmailService {
+
+
+//    // for example, smtp.mailgun.org
+//    private static final String SMTP_SERVER = "smtp server ";
+//    private static final String USERNAME = "chinkang.no.reply@gmail.com";
+//    private static final String PASSWORD = "chinkang123";
+//
+//    private static final String EMAIL_FROM = "From@gmail.com";
+//    private static final String EMAIL_TO = "howard.zhang.1995@gmail.com";
+//    private static final String EMAIL_TO_CC = "";
+//
+//    private static final String EMAIL_SUBJECT = "Test Send Email via SMTP";
+//    private static final String EMAIL_TEXT = "Hello Java Mail \n ABC123";
+
+    public void start() {
+
+        final String username  = "chinkang.no.reply@gmail.com";
+        final String password  = "chinkang123";
+
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+
+
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("howard.zhang.1995@gmail.com")
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
