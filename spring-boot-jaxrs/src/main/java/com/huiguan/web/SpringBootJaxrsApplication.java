c3486a2455a466c419b5930552470931aa7ed91e
e0008233 <howard.zhang.1995@gmail.com>
2019-12-28 00:04:14 +0800

    update api

--- spring-boot-jaxrs/src/main/java/com/huiguan/web/SpringBootJaxrsApplication.java
+++ spring-boot-jaxrs/src/main/java/com/huiguan/web/SpringBootJaxrsApplication.java
@@ -2,9 +2,20 @@ package com.huiguan.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class SpringBootJaxrsApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringBootJaxrsApplication.class, args);
+//	}
+//
+//}
+
 
 @SpringBootApplication
-public class SpringBootJaxrsApplication {
+public class SpringBootJaxrsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJaxrsApplication.class, args);
